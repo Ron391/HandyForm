52a61ee463fc5d09cbf7882f1d8be73cea039f8d
Ron391 <75159085+Ron391@users.noreply.github.com>
2021-07-19 17:33:17 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using HandyControl.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandyForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConfigHelper.Instance.SetLang("en");
+            D1.SelectedDateTime = DateTime.Now;
+            D2.SelectedDateTime = DateTime.Now;
+
+
+        }
+    }
+}
